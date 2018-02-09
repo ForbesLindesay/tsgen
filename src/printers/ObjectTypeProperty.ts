71ea642048d90756ecb2be7de6c42a5fc904bd33
Forbes Lindesay <forbes@lindesay.co.uk>
2018-02-09 17:42:18 +0000

    Fix handling of optional properties
    [closes #4]

--- src/printers/ObjectTypeProperty.ts
+++ src/printers/ObjectTypeProperty.ts
@@ -6,11 +6,15 @@ export default function printObjectTypeProperty(
   node: bt.ObjectTypeProperty,
   ctx: Context,
 ): string {
+  const optional: boolean = (node as any).optional;
   if (bt.isStringLiteral(node.key)) {
-    return `${JSON.stringify(node.key.value)}: ${print(node.value, ctx)},`;
+    return `${JSON.stringify(node.key.value)}${optional ? '?' : ''}: ${print(
+      node.value,
+      ctx,
+    )},`;
   }
   if (bt.isIdentifier(node.key)) {
-    return `${node.key.name}: ${print(node.value, ctx)},`;
+    return `${node.key.name}${optional ? '?' : ''}: ${print(node.value, ctx)},`;
   }
   throw ctx.getError(
     'Expected the key of this type property to be an Identifier or a StringLiteral.',
